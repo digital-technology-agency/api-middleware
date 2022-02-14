a22b4a4f37de522a801b87d916105509b722d2a5
deemak <deema_k@mail.ru>
2022-02-14 21:02:19 +0300

    basic auth

---
+++ cmd/api/api-svc.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var authData = auth{
+	User:     "user",
+	Password: "user123",
+}
+
+type auth struct {
+	User     string
+	Password string
+}
+
+type Response struct {
+	Status  int
+	Service string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1", health)
+	mux.HandleFunc("/api/v1/protected", basicAuth(protected))
+	s := &http.Server{
+		Addr:         ":9999",
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 15,
+	}
+	log.Printf("Starting server on %s", s.Addr)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Err %v\n", err.Error())
+	}
+}
+
+func basicAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		(w).Header().Set("Content-Type", "application/json")
+		user, password, ok := r.BasicAuth()
+		if !ok {
+			(w).WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if authData.User != user || authData.Password != strings.ReplaceAll(password, "\n", "") {
+			(w).WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		(w).WriteHeader(http.StatusOK)
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Content-Type", "application/json")
+	(w).WriteHeader(http.StatusOK)
+	dataBytes, _ := json.Marshal(Response{Status: http.StatusOK, Service: "health"})
+	(w).Write(dataBytes)
+}
+
+func protected(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Content-Type", "application/json")
+	(w).WriteHeader(http.StatusOK)
+	dataBytes, _ := json.Marshal(Response{Status: http.StatusOK, Service: "protected"})
+	(w).Write(dataBytes)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/digital-technology-agency/api-middleware
+
+go 1.17
